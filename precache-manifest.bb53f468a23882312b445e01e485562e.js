8e9d622e03d439cce36c12deede70950851380e6
Meriç Çağlar <mrcaglar6@gmail.com>
2020-10-17 21:36:23 +0300

    Updates

---
+++ precache-manifest.bb53f468a23882312b445e01e485562e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f86d7f22fa8c127c5124ce732a0bc50",
+    "url": "/react-color-palette/index.html"
+  },
+  {
+    "revision": "7a8273bcdaa05461a7f2",
+    "url": "/react-color-palette/static/css/main.7f896ec5.chunk.css"
+  },
+  {
+    "revision": "a7c14f8dca6dd2347e94",
+    "url": "/react-color-palette/static/js/2.343e0bfe.chunk.js"
+  },
+  {
+    "revision": "6d2a1dace76b769103ba1a47eaa90117",
+    "url": "/react-color-palette/static/js/2.343e0bfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a8273bcdaa05461a7f2",
+    "url": "/react-color-palette/static/js/main.36edd23b.chunk.js"
+  },
+  {
+    "revision": "8a5c3a6694cf06ff00cf",
+    "url": "/react-color-palette/static/js/runtime-main.3fe00537.js"
+  }
+]);
